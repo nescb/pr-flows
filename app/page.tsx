6079fa146aafc301885226cd6d1e7a551b7d8fb0
Nestor Carvantes <nestorba@amazon.com>
2026-02-13 17:05:28 +0000

    yet another commit

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            PR test from a fork!!!!! Looking for a starting point or more instructions? Head over to{" "}
+            PR test from a fork!!!!! Build again!! Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
